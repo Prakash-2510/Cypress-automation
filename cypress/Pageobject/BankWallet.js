bd10ea20a7b137dbc84a89aaa8015050fce6c369
Prakash <olkaiyap2510@gmail.com>
2024-10-24 15:12:19 +0530

    Bankwallet test cases complted

--- cypress/Pageobject/BankWallet.js
+++ cypress/Pageobject/BankWallet.js
@@ -4,20 +4,61 @@ class Bankwallet
    txtusername="#username";
    txtpassword="#password";
    txtxbtn="#log-in";
+   txtprofilename=".logged-user-name";
+   txtsearchbutton=".element-search.autosuggest-search-activator";
+   txtcardtype1="li[class='selected has-sub-menu'] a span";
+   txtcardtype2="body > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > ul:nth-child(3) > li:nth-child(3) > a:nth-child(1) > span:nth-child(2)"
+   txttime="#time";
+   txtaddaccount="a[class='btn btn-primary btn-sm'] span";
+   txtmakepayment="a[class='btn btn-success btn-sm'] span";
+   txtpending="tbody tr:nth-child(3) td:nth-child(1) span:nth-child(2)"
 
    enterusername(username)
       {
       cy.get(this.txtusername).type(username);
       }
+
    enterpassword(password)
        {
         cy.get(this.txtpassword).type(password);
        }
+
    clicklogin()
        {
        cy.get(this.txtxbtn).click();
        }
- 
+   userprofile()
+       {
+        cy.get(this.userprofile).contains('Jack Gomez');
+       }
+
+    searchbutton()
+       {
+        cy.get(this.txtsearchbutton).should('exist')
+       }
+     cardtype()
+       {
+       cy.get(this.txtcardtype1).contains('Credit cards')
+       cy.get(this.txtcardtype2).contains('Debit cards')
+       }
+    banktime()
+       {
+        cy.get(this.txttime).contains('Your nearest branch closes in')
+       }
+    addbankaccount()  
+       {
+       cy.get(this.txtaddaccount).should('be.visible');
+       } 
+    makepayment()
+       {
+        cy.get(this.txtmakepayment).should('be.visible');
+       }
+    pendingpayment()
+       {
+        cy.get(this.txtpending).should('be.visible');
+       }   
+    
+
 
 
 }
--- cypress/e2e/Bankwallet.cy.js
+++ cypress/e2e/Bankwallet.cy.js
@@ -3,16 +3,70 @@ import Bankwallet from "../Pageobject/BankWallet";
 describe('Bank Wallet', () => {
     beforeEach(() => {
         cy.visit('https://demo.applitools.com')
-
     });
     it('Positive login', () => {
-
         const ln=new Bankwallet();
         ln.enterusername("Prakash");
         ln.enterpassword("Yug");
         ln.clicklogin()
-    
-            
+
         });
+        it('User Profile Verify', () => {
+            const ln=new Bankwallet();
+            ln.enterusername("Prakash");
+            ln.enterpassword("Yug");
+            ln.clicklogin();
+            ln.userprofile();
+        });
+        it('Search button show to user', () => {
+            const ln=new Bankwallet();
+            ln.enterusername("Prakash");
+            ln.enterpassword("Yug");
+            ln.clicklogin();
+            ln.searchbutton()
+        });
+
+        it('Verify the two card type', () => {
+            const ln=new Bankwallet();
+            ln.enterusername("Prakash");
+            ln.enterpassword("Yug");
+            ln.clicklogin();
+            ln.cardtype()
+        });
+
+        it('Bank close time', () => {
+            const ln=new Bankwallet();
+            ln.enterusername("Prakash");
+            ln.enterpassword("Yug");
+            ln.clicklogin();
+            ln.banktime()
+        });
+        it('Add account button verify', () => {
+            const ln=new Bankwallet();
+            ln.enterusername("Prakash");
+            ln.enterpassword("Yug");
+            ln.clicklogin();
+            ln.addbankaccount()
+        });
+        it('Make payment button verify', () => {
+            const ln=new Bankwallet();
+            ln.enterusername("Prakash");
+            ln.enterpassword("Yug");
+            ln.clicklogin();
+            ln.makepayment()
+      
+        });
+        it('Make payment button verify', () => {
+            const ln=new Bankwallet();
+            ln.enterusername("Prakash");
+            ln.enterpassword("Yug");
+            ln.clicklogin();
+            ln.pendingpayment()
+      
+        });
+
+
+
+       
    
 });
